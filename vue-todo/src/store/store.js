72b3fae1da8184e58ea1d73ec62aec9ababb5f17
kkwongs <kihyun5741@gmail.com>
2021-01-12 20:34:14 +0900

    mutation 적용

--- vue-todo/src/store/store.js
+++ vue-todo/src/store/store.js
@@ -21,5 +21,28 @@ export const store = new Vuex.Store({
   // state(data), getters(computed), mutations(methods), actions(aysnc methods)
   state: {
     todoItems: storage.fetch()
+  },
+  mutations: {
+    addOneItem(state, todoItem) {
+      var obj = {
+        completed: false,
+        item: todoItem,
+      };
+      localStorage.setItem(todoItem, JSON.stringify(obj));
+      state.todoItems.push(obj);
+    },
+    removeOneItem(state, payload) {
+      localStorage.removeItem(payload.todoItem.item);
+      state.todoItems.splice(payload.index, 1);
+    },
+    toggleOneItem(state, payload) {
+      state.todoItems[payload.index].completed = !state.todoItems[payload.index].completed;
+      localStorage.removeItem(payload.todoItem.item);
+      localStorage.setItem(payload.todoItem.item, JSON.stringify(payload.todoItem));
+    },
+    clearAllItems(state) {
+      localStorage.clear();
+      state.todoItems = [];
+    }
   }
 })
