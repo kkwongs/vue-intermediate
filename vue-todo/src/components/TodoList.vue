a383c64bba83c70f46248abc4e1cc7dfa6c99cb4
kkwongs <43363739+kkwongs@users.noreply.github.com>
2021-01-13 17:09:31 +0900

    Vuex (#4)
    * state, getters 적용
    
    * mutation 적용
    
    * 헬퍼 함수 적용
    
    * 스토어 파일 분리
    
    * 모듈화

--- vue-todo/src/components/TodoList.vue
+++ vue-todo/src/components/TodoList.vue
@@ -1,15 +1,15 @@
 <template>
   <div>
     <transition-group name="list" tag="ul">
-      <li v-for="(todoItem, index) in propsdata" v-bind:key="todoItem.item" class="shadow">
+      <li v-for="(todoItem, index) in this.storedTodoItems" v-bind:key="todoItem.item" class="shadow">
         <i 
           class="fas fa-check checkBtn" 
-          v-bind:class="{checkBtnCompleted:todoItem.completed}" v-on:click="toggleComplete(todoItem, index)">
+          v-bind:class="{checkBtnCompleted:todoItem.completed}" v-on:click="toggleComplete({todoItem, index})">
         </i>
         <span v-bind:class="{textCompleted:todoItem.completed}">
           {{ todoItem.item }}
         </span>
-        <span class="removeBtn" v-on:click="removeTodo(todoItem, index)">
+        <span class="removeBtn" v-on:click="removeTodo({todoItem, index})">
           <i class="fas fa-trash-alt"></i>
         </span>
       </li>
@@ -18,16 +18,18 @@
 </template>
 
 <script>
+import { mapGetters, mapMutations } from 'vuex'
+
 export default {
-  props: ['propsdata'],
   methods: {
-    removeTodo(todoItem, index) {
-      this.$emit('removeItem', todoItem, index);
-    },
-    toggleComplete(todoItem, index) {
-      this.$emit('toggleItem', todoItem, index);
-    }
+    ...mapMutations({
+      removeTodo: 'removeOneItem',
+      toggleComplete: 'toggleOneItem'
+    }),
   },
+  computed: {
+    ...mapGetters(['storedTodoItems'])
+  }
 }
 </script>
 
