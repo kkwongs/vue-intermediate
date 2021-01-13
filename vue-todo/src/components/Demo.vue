a63b91127e8ff1ac3a7e8dd50a1bb9fb09418f1d
kkwongs <kihyun5741@gmail.com>
2021-01-13 16:45:28 +0900

    헬퍼 함수 적용

---
+++ vue-todo/src/components/Demo.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="root">
+    <p>{{ originalPrice }}</p>
+    <p>{{ doublePrice }}</p>
+    <p>{{ triplePrice }}</p>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+
+export default {
+  computed: {
+    ...mapGetters(['originalPrice', 'doublePrice', 'triplePrice']),
+    
+    // originalPrice() {
+    //   return this.$store.gettes.originalPrice;
+    // },
+    // doublePrice() {
+    //   return this.$store.gettes.doublePrice;
+    // },
+    // triplePrice() {
+    //   return this.$store.gettes.triplePrice;
+    // }
+  }
+}
+</script>
+
+<style>
+
+</style>
